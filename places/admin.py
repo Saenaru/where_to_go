909cb0e7957c73bcdac71d4a7c26226966a2a067
Saenaru <96067905+Saenaru@users.noreply.github.com>
2025-08-01 16:58:52 +0300

    I hid the numbering and added changes to the photo order

--- places/admin.py
+++ places/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from .models import Place, Image
+from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminBase
 
-class ImageInline(admin.TabularInline):
+class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
     extra = 1
-    fields = ('image_url', 'image', 'position', 'preview')
+    fields = ('image_url', 'image', 'preview')
     readonly_fields = ('preview',)
+    ordering = ('position',)
 
     def preview(self, obj):
         if obj.image:
@@ -14,14 +16,14 @@ class ImageInline(admin.TabularInline):
         return "Нет изображения"
 
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     inlines = [ImageInline]
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('place', 'position', 'preview')
     list_editable = ('position',)
-    fields = ('place', 'image_url', 'image', 'position', 'preview')
+    fields = ('place', 'image_url', 'image', 'preview')
     readonly_fields = ('preview',)
 
     def preview(self, obj):
